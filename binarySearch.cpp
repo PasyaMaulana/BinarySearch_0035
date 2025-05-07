149cb08a929665bb4d9ed4ff450d950343d12838
PasyaMaulana <pasyamaulana17@gmail.com>
2025-05-07 19:53:23 +0700

    pengisian array elemen i menggunakan looping for

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -18,11 +18,17 @@ void input()
         }
         else
         {
-            cout << "\nJumlah elemen tidak boleh lebih dari 10. Silakan coba lagi.\n";
+            cout << "\n[!] Jumlah elemen tidak boleh lebih dari 10. Silakan coba lagi.\n";
         }
     }
 
     cout << "==============================" << endl;
     cout << "Masukkan Elemen Array" << endl;
     cout << "==============================" << endl;
+
+    for (int i = 0; i < nPanjang; i++)
+    {
+        cout << "Data ke-" << (i + 1) << "= ";
+        cin >> element[i];
+    }
 }
